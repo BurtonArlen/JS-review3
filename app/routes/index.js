61cb1701e804b81b517e3fd27ec807972f4afc0e
ab <burton.arlen.1990@gmail.com>
2016-06-16 09:10:28 -0700

    basic functionality built

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return this.store.findAll('question');
+  },
+  actions: {
+    save3(params){
+      var newQuestion = this.store.createRecord('rental', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    }
+  }
 });
