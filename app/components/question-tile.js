95923d4bc9445176311b67abf97452cedd72c5d5
ab <burton.arlen.1990@gmail.com>
2016-06-16 08:58:44 -0700

    answer show hide functionality added

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isAnswerShow: false,
+  actions: {
+    answerShow: function(){
+      this.set('isAnswerShow', true);
+    },
+    answerHide: function(){
+      this.set('isAnswerShow', false);
+    }
+  }
 });
